cd66ff0e8f94a3a1517fa38d4b306b816a98ae56
Bright Atsighi <brytebee@gmail.com>
2024-04-05 12:24:24 +0100

    Added card style and image

--- src/components/product/ProductToCart.tsx
+++ src/components/product/ProductToCart.tsx
@@ -5,21 +5,18 @@ import FormButton from "../common/form-button";
 import { createCart, createCartItem } from "@/actions";
 import { useSession } from "next-auth/react";
 import { cartStore } from "@/store";
+import Image from "next/image";
+import { Product } from "@prisma/client";
 
-interface Props {
-  prod: {
-    id: string;
-    slug: string;
-    description: string;
-    quantity: number;
-    price: number;
-    categoryId: string;
-    createdAt: Date;
-    updatedAt: Date;
-  };
+type Props = Product & {
+  images: { url: string | null; productId: string }[];
+};
+
+interface ProdProps {
+  prod: Props;
 }
 
-export default function ProductToCart({ prod }: Props) {
+export default function ProductToCart({ prod }: ProdProps) {
   const session = useSession();
   const { addItem } = cartStore();
   const [error, setError] = useState(null);
@@ -73,35 +70,46 @@ export default function ProductToCart({ prod }: Props) {
   };
 
   return (
-    <form onSubmit={handler} className="mb-4">
-      <p>{prod.slug}</p>
-      <p>{prod.description}</p>
-      <p className="mb-2">{prod.price}</p>
+    <form onSubmit={handler} className="mb-4 border rounded-lg shadow-md p-4">
+      <div className="flex items-center mb-4">
+        <Image
+          // @ts-ignore
+          src={prod.images[0].url}
+          alt={prod.slug}
+          width={100}
+          height={100}
+          className="rounded-lg mr-4"
+        />
+        <div>
+          <p className="text-lg font-semibold">{prod.slug}</p>
+          <p className="text-gray-600">{prod.description.slice(0, 20)}</p>
+          <p className="text-gray-800 font-semibold">${prod.price}</p>
+        </div>
+      </div>
 
       {prod.quantity > 1 ? (
         <>
-          <div className="my-2">
+          <div className="flex items-center my-2 h-fit">
             <input
               type="number"
               name="quantity"
               min={1}
               max={prod.quantity}
               defaultValue={1}
+              className="w-16 py-1 px-2 border border-gray-300 rounded-md mr-2"
             />
-            {prod.quantity}
+            <p className="text-gray-600">In stock: {prod.quantity}</p>
           </div>
           {/* @ts-ignore */}
-          {error && <p className=" text-red-200">{error._form}</p>}
+          {error && <p className="text-red-500">{error._form}</p>}
           {session.status === "authenticated" && (
             <FormButton>Add to Cart</FormButton>
           )}
         </>
       ) : (
-        <>
-          <span className="outline outline-4 outline-red-800 bg-yellow-600 p-2 my-4 rounded-md">
-            Out of stock
-          </span>
-        </>
+        <div className="bg-yellow-100 text-yellow-800 px-4 py-2 rounded-md">
+          Out of stock
+        </div>
       )}
     </form>
   );
