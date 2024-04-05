7c8f719770619160bb2a40afbbb181f51f5195dc
Bright Atsighi <brytebee@gmail.com>
2024-04-05 15:40:56 +0100

    Updated store to track ids already in cart, only add if not existing in cart

--- src/store/cartStore.ts
+++ src/store/cartStore.ts
@@ -1,15 +1,22 @@
 import { create } from "zustand";
 
 interface Cart {
-  count: number;
-  addItem: () => void;
-  removeItem: () => void;
-  setcount: () => void;
+  ids: string[];
+  addItem: (id: string) => void;
+  removeItem: (id: string) => void;
 }
 
 export const cartStore = create<Cart>()((set) => ({
-  count: 0,
-  addItem: () => set((state) => ({ count: state.count + 1 })),
-  removeItem: () => set((state) => ({ count: state.count - 1 })),
-  setcount: () => set((state) => ({ count: state.count + 1 })),
+  ids: [],
+  addItem: (id) =>
+    set((state) => {
+      const exists = state.ids.includes(id);
+      return {
+        ids: exists ? state.ids : [...state.ids, id],
+      };
+    }),
+  removeItem: (id: string) =>
+    set((state) => ({
+      ids: state.ids.filter((itemId) => itemId !== id),
+    })),
 }));
